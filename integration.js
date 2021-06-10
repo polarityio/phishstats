3e621a5e285483c9577f059a3885faddd0e2e59b
Nicholas Smith <nick@polarity.io>
2021-06-10 12:50:13 -0700

    encoded uri param values

--- integration.js
+++ integration.js
@@ -4,15 +4,17 @@ const gaxios = require('gaxios');
 const https = require('https');
 const config = require('./config/config');
 const errorToPojo = require('./utils/errorToPojo');
-const URL = `https://phishstats.info:2096/api/phishing?_where=`;
 
+const URL = `https://phishstats.info:2096/api/phishing?`;
 const _configFieldIsValid = (field) => typeof field === 'string' && field.length > 0;
 let Logger;
+
 const startup = (logger) => {
   Logger = logger;
   const {
     request: { ca, cert, key, passphrase, rejectUnauthorized, proxy }
   } = config;
+
   const httpsAgent = new https.Agent({
     ...(_configFieldIsValid(ca) && { ca: fs.readFileSync(ca) }),
     ...(_configFieldIsValid(cert) && { cert: fs.readFileSync(cert) }),
@@ -25,55 +27,76 @@ const startup = (logger) => {
     ...(_configFieldIsValid(proxy) && { proxy: { host: proxy } })
   };
 };
+
 const doLookup = async (entities, options, cb) => {
   let lookupResults;
+
   try {
     lookupResults = await async.parallelLimit(
       entities.map((entity) => async () => {
-        const lookupResult = await lookUpEntity(entity);
+        const lookupResult = await lookUpEntity(entity, options);
         return lookupResult;
       }),
       10
     );
   } catch (err) {
-    cb(errorToPojo(err), null);
+    let detailMsg = 'There was an unexpected error';
+
+    if (err.response) {
+      detailsMsg = `Received unexpected HTTP status ${err.response.status}`;
+    } else if (err.request) {
+      detailsMsg = `There was an HTTP err`;
+    } else {
+      detailMsg = err.message;
+    }
+    return cb(errorToPojo(detailMsg, err));
   }
+
   Logger.trace({ lookupResults }, 'Lookup results');
   return cb(null, lookupResults);
 };
-const lookUpEntity = async (entity, done) => {
+
+const lookUpEntity = async (entity, options) => {
   let results;
-  let res = {};
+
   const buildEntityQuery = {
     IPv4: (entityValue) =>
-      `(ip,like,${entityValue})~or(bgp,like,${entityValue})&_size=20`,
+      `_where=(ip,eq,${entityValue})~or(bgp,eq,${entityValue})&_size=10`,
     IPv6: (entityValue) =>
-      `(ip,like,${entityValue})~or(bgp,like,${entityValue})&_size=20`,
+      `_where=(ip,eq,${entityValue})~or(bgp,eq,${entityValue})&_size=10`,
     domain: (entityValue) =>
-      `(host,like,${entityValue})~or(domain,like,${entityValue})&_size=20`,
-    SHA256: (entityValue) => `(hash,like,${entityValue})&_size=20`,
-    url: (entityValue) => `(url,like,${entityValue})&_size=20`
+      `(host,eq,${entityValue})~or(domain,eq,${entityValue})&_size=10`,
+    hash: (entityValue) => `(hash,eq,${entityValue})&_size=10`
   };
-  try {
-    if (buildEntityQuery[entity.type]) {
-      results = await gaxios.request({
-        url: URL,
-        params: {
-          querystring: buildEntityQuery[entity.type](entity.value)
+
+  if (buildEntityQuery[entity.type]) {
+    results = await gaxios.request({
+      url: URL + buildEntityQuery[entity.type](entity.value),
+      params: {
+        querystring: {
+          type: entity.type,
+          value: entity.value
         }
-      });
-    }
-  } catch (err) {
-    if (err || results.status !== 200) {
-      return err || results;
-    }
+      }
+    });
+
+    return (lookupResult = {
+      entity,
+      data:
+        Array.isArray(results.data) && results.data.length > 0
+          ? { summary: getSummary(results.data), details: results.data }
+          : null
+    });
   }
-  res.entity = entity;
-  res.data = results.data
-    ? { summary: [`Phish Results: ${results.data.length}`], details: results.data }
-    : null;
-  return res;
 };
+
+const getSummary = (data) => {
+  let tags = [];
+  const totalResults = data.length;
+  tags.push(`Results: ${totalResults}`);
+  return tags;
+};
+
 module.exports = {
   doLookup,
   startup
