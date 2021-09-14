a1cc1afb5c7e67a986545ea51122eabf939a6e71
Jon Penwood <jonp@polarity.io>
2021-09-14 12:06:03 -0600

    Updated moment-formatter timezone

--- components/block.js
+++ components/block.js
@@ -1,4 +1,7 @@
 'use strict';
 polarity.export = PolarityComponent.extend({
   details: Ember.computed.alias('block.data.details'),
+  timezone: Ember.computed("Intl", function () {
+    return Intl.DateTimeFormat().resolvedOptions().timeZone;
+  })
 });
